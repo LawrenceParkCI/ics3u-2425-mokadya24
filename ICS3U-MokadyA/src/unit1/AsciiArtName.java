8b4cbc9ead64aa28499ed7cd06e5d0bfe2377f61
daphn <daphn@10.123.162.57>
2024-09-26 11:11:24 -0400

    Ascii Art Signature Completed

---
+++ ICS3U-MokadyA/src/unit1/AsciiArtName.java
@@ -0,0 +1,44 @@
+package unit1;
+/**
+ * Description: This is my name (Amir Mokady) in Ascii art.<br> I used the Ghost font.<br><br>
+ * Date: September, 26, 2024
+ * @author Amir Mokady
+ */
+public class AsciiArtName {
+
+	public static void main(String[] args) {
+		//name in Ascii art
+		System.out.println("   ('-.     _   .-')            _  .-')         _   .-')                .-. .-')    ('-.     _ .-') _               ");
+		System.out.println("  ( OO ).-.( '.( OO )_         ( \\( -O )       ( '.( OO )_              \\  ( OO )  ( OO ).-.( (  OO) )              ");
+		System.out.println("  / . --. / ,--.   ,--.) ,-.-') ,------.        ,--.   ,--.).-'),-----. ,--. ,--.  / . --. / \\     .'_   ,--.   ,--.");
+		System.out.println("  | \\-.  \\  |   `.'   |  |  |OO)|   /`. '       |   `.'   |( OO'  .-.  '|  .'   /  | \\-.  \\  ,`'--..._)   \\  `.'  / ");
+		System.out.println(".-'-'  |  | |         |  |  |  \\|  /  | |       |         |/   |  | |  ||      /,.-'-'  |  | |  |  \\  ' .-')     /  ");
+		System.out.println(" \\| |_.'  | |  |'.'|  |  |  |(_/|  |_.' |       |  |'.'|  |\\_) |  |\\|  ||     ' _)\\| |_.'  | |  |   ' |(OO  \\   /   ");
+		System.out.println("  |  .-.  | |  |   |  | ,|  |_.'|  .  '.'       |  |   |  |  \\ |  | |  ||  .   \\   |  .-.  | |  |   / : |   /  /\\_  ");
+		System.out.println("  |  | |  | |  |   |  |(_|  |   |  |\\  \\        |  |   |  |   `'  '-'  '|  |\\   \\  |  | |  | |  '--'  / `-./  /.__) ");
+		System.out.println("  `--' `--' `--'   `--'  `--'   `--' '--'       `--'   `--'     `-----' `--' '--'  `--' `--' `-------'    `--'      ");
+	
+		//Drawing of a person
+		System.out.println("\n\n\n              &&&&&&&&,             ");
+		System.out.println("*&&&&#       %&&&&&&&&&,      .@&&&@ ");
+		System.out.println(" /@&&&&&&%   ,&&&&&&&&@   .@&&&&&&&.");
+		System.out.println("     /@&&&&&&%  %@@@# .@&&&&&&&     ");
+		System.out.println("         *@&&&&&&&&&&&&&&&&         ");
+		System.out.println("             #&&&&&&&&&.            ");
+		System.out.println("             #&&&&&&&&&.            ");
+		System.out.println("             #&&&&&&&&&.            ");
+		System.out.println("             #&&&&&&&&&.            ");
+		System.out.println("             #&&&&&&&&&.            ");
+		System.out.println("             &&&&&&&&&&/             ");
+		System.out.println("            .&&&&@,&&&&@             ");
+		System.out.println("            %&&&&/ @&&&&*            ");
+		System.out.println("            &&&&@  ,&&&&@            ");
+		System.out.println("           &&&&&#   @&&&&.          ");
+		System.out.println("           @&&&&    *&&&&&          ");
+		System.out.println("          @&&&&&     @&&&&           ");
+		System.out.println("          &&&&@      (&&&&@          ");
+		System.out.println("          &&&&&       @&&&@          ");
+
+	}
+
+}
